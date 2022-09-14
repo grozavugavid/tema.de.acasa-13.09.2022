c78150aa5bd247d543a7e54839928fc8abe0c89b
grozavudavid <70814477+grozavugavid@users.noreply.github.com>
2022-09-14 12:57:10 +0300

    Add files via upload

---
+++ problema 1.py
@@ -0,0 +1,8 @@
+def func():
+    return 1+0.5**2
+
+    def putere(x,y):
+        return x**y
+
+print(putere(0.5,2))
+print('s=', putere(0.5,0)+putere(0.5,2)+putere(0.5,4)+putere(0.5,6)+putere(0.5,8))
---
+++ problema 2.py
@@ -0,0 +1,11 @@
+def fact(x):
+    c=1
+    for i in range (1,x+1)
+    c=c*x
+    return c
+n=int(input('introdu n' )
+m=int(input('introdu m'))
+if n<m:
+    print('eroare')
+    else:
+        print('c=', fact(n)/fact(m)*fact(m-n))
